ef6678859971856b5516ca9d9d22cfd2a044a5bb
Basak Deveci <D22124576@mytudublin.ie>
2025-09-26 00:03:41 +0100

    Tailwind is added with required files

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,45 +1,18 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
-
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+import { TextInput } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import './global.css';
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+      <SafeAreaView>
+        <TextInput
+          className='bg-pink-400'
+          placeholder='Enter your name'
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome to My Wardrobe!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-    </ParallaxScrollView>
+        <TextInput
+          className='bg-pink-400'
+          placeholder='Enter your name'
+        />
+      </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
