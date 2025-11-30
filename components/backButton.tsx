b5190b63f39b9b3e10f4b52305ec1eb14100e1d7
Basak Deveci <D22124576@mytudublin.ie>
2025-11-30 11:02:24 +0000

    Back button component has been created

--- components/backButton.tsx
+++ components/backButton.tsx
@@ -1,17 +1,27 @@
-import { TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-import { Stack, useRouter } from "expo-router";
-import React from "react";
+import { usePathname, useRouter } from "expo-router";
+import { TouchableOpacity } from "react-native";
 
-export default function BackButton() {
+
+interface BackButtonProps {
+  containerStyle?: object;
+}
+
+export default function BackButton({containerStyle = {}}: BackButtonProps) {
   const router = useRouter();
+  const pathname = usePathname();
+
+  const handleBack = () => {
+    if (pathname === "/image-gallery-view") {
+      router.push("/wardrobe");   // always go to wardrobe
+      return;
+    }
+    router.back();
+  };
 
   return (
-    <TouchableOpacity
-      onPress={() => router.back()}
-      className="absolute top-12 left-5 p-2 rounded-full"
-    >
-      <Ionicons name="chevron-back" size={26} color="#050605ff" />
+    <TouchableOpacity onPress={handleBack} style={containerStyle}>
+      <Ionicons name="arrow-back-outline" size={24} color="black" />
     </TouchableOpacity>
   );
 }
